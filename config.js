04487bbff21232997f70f1ed6ddc408af941a269
lalunamalia <nur.amalia15933@guru.smp.belajar.id>
2026-06-18 17:24:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-// GANTI dengan URL Web App Google Apps Script setelah deploy.
+// Isi dengan URL Web App Google Apps Script setelah deploy.
 // Contoh: https://script.google.com/macros/s/AKfycbxxxxxxxx/exec
-window.NAFA_API_URL = "https://script.google.com/macros/s/AKfycbw0I8TbHQKxhPp282nhSJklOzZMdhtOh_G_Op65At8SfAtfAh2ya8X5XCNe54_UWXuc2g/exec";
+window.NAFA_API_URL = "https://script.google.com/macros/s/AKfycbwp26lQOTVh3BJ2jO9u8fQbaiYalN5eZyqli12u9ZZWtmD1DoO7XfshmEGg8JEHcEgicA/exec";
